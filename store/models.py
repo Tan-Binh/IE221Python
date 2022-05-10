bb1bc59f0eefa7e76be4c0bd45237c43363aaa6d
Khoa-Luong <khoaluong117@gmail.com>
2022-05-10 22:50:30 +0700

    Thêm dữ liệu

--- store/models.py
+++ store/models.py
@@ -7,7 +7,7 @@ from django.db import models
 class Product(models.Model):
     product_name = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    description = models.TextField(max_length=500, blank=True)
+    description = models.TextField(max_length=5000, blank=True)
     price = models.IntegerField()
     images = models.ImageField(upload_to='photos/products')
     stock = models.IntegerField()
