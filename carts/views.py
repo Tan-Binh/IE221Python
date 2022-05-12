3f6708942afe2074ca1421a938c7e8ecf3bacaf6
Tan-Binh <phantanbinh0505@gmail.com>
2022-05-12 10:04:10 +0700

    Sửa thanh toán
    Sửa thanh toán khi chưa đăng nhập

--- carts/views.py
+++ carts/views.py
@@ -183,7 +183,7 @@ def cart(request, total=0, quantity=0, cart_items=None):
 @login_required(login_url='login')
 def checkout(request, total=0, quantity=0, cart_items=None):
     try:
-        cart = Cart.objects.get(cart_id=_cart_id(request=request))
+        # cart = Cart.objects.get(cart_id=_cart_id(request=request))
         cart_items = CartItem.objects.filter(user=request.user, is_active=True)
         for cart_item in cart_items:
             total += cart_item.product.price * cart_item.quantity
